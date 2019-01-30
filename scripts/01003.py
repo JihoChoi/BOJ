e3fb6a71b3fe01993f05b39998931ad4f0632233
JihoChoi <jihochoi1993@gmail.com>
2019-01-30 16:54:02 +0900

    Fix linked list error

--- scripts/01003.py
+++ scripts/01003.py
@@ -1,11 +1,9 @@
 # Dynamic Programming (동적 계획법)
 #   DP, memoization, fibonacci
 
-
 # References
 # https://pyformat.info/
 # https://github.com/soakdma37/BOJ
-
 
 import sys
 
@@ -13,24 +11,21 @@ T = int(input())
 MAX = 40
 
 # dp, memo, cache, ...
-dp = [{'zero': 0, 'one': 0}] * (MAX + 1)
-dp[0] = {'zero': 1, 'one': 0}
-dp[1] = {'zero': 0, 'one': 1}
+# dp = [{'zero': 0, 'one': 0}] * (MAX + 1)
 
-# print('{} {}'.format(dp[0]['zero'], dp[0]['one']))
-# print('{} {}'.format(dp[1]['zero'], dp[1]['one']))
-# print(dp)
+# List in Python is not array, linked list (pointer array)
+dp = []
+dp.append({'zero': 1, 'one': 0}) # dp[0]
+dp.append({'zero': 0, 'one': 1}) # dp[1]
 
-# for i in range(2, MAX + 1):
-for i in range(2, 4):
-    dp[i]['zero'] = dp[i-2]['zero'] + dp[i-1]['zero']
-    dp[i]['one'] = dp[i-2]['one'] + dp[i-1]['one']
+for i in range(2, MAX + 1):
+    dic = {}
+    dic['zero'] = dp[i-2]['zero'] + dp[i-1]['zero']
+    dic['one'] = dp[i-2]['one'] + dp[i-1]['one']
 
-    print(dp[i]['one'], dp[i-2]['one'], dp[i-1]['one'])
-    print(dp[i-2]['zero'], "+", dp[i-1]['zero'])
-    print(dp[i-2]['one'], "+", dp[i-1]['one'])
-    print('{:02d} {} {}'.format(i, dp[i]['zero'], dp[i]['one']))
+    dp.append(dic) # dp[i]
+    # print('{:02d} {} {}'.format(i, dp[i]['zero'], dp[i]['one']))
 
-
-# for _ in range(T):
-#     print('{} {}'.format(dp[i]['zero'], dp[i]['one']))
+for _ in range(T):
+    i = int(input())
+    print('{} {}'.format(dp[i]['zero'], dp[i]['one']))
